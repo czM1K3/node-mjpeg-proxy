114eecc32e82b3c293e5709f915caf0b6301b127
czM1K3 <misamadera1@gmail.com>
2021-03-12 16:23:15 +0100

    Changed

---
+++ index.js
@@ -0,0 +1,16 @@
+const MjpegProxy = require('./mjpeg-proxy').MjpegProxy;
+const express = require('express');
+const fs = require('fs');
+
+const config = JSON.parse(fs.readFileSync('config.json'));
+
+const app = express();
+
+config.cams.map(x => {
+    app.get('/' + x.name, new MjpegProxy(x.address).proxyRequest);
+    console.log("Adding camera: " + x.name);
+});
+
+app.listen(config.port);
+
+console.log("Listening on port " + config.port);
